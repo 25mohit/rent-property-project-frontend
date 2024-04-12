0d99632bf04fd9c1d1b38212a16f3aaae04cf511
25mohit <mohit724agarwal@gmail.com>
2024-04-12 22:57:33 +0530

    'Not-Found' Page added for unrechable routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+  return (
+    <div className="not-found">
+      <h1>404: Not Found</h1>
+      <p>Oops! Looks like the page you requested couldn't be found.</p>
+      <a href="/">Go back home</a>
+    </div>
+  );
+}
