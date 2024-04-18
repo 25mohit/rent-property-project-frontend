ac0a00a881f3c3bcf64b0f87b2dad4a68fcf68c8
25mohit <mohit724agarwal@gmail.com>
2024-04-18 09:04:35 +0530

    Changes Done

--- src/app/components/HOC/Layout/SingleContentLayout.tsx
+++ src/app/components/HOC/Layout/SingleContentLayout.tsx
@@ -18,12 +18,6 @@ const SingleContentLayout:React.FC<SingleContentInterface> = ({ children }) => {
           setHeight(window.innerHeight);
           return () => window.removeEventListener('resize', onResize);
       }
-    const onResize = () => {
-      const containerHTML = document.querySelector('.singleContentLayout');
-      (containerHTML as HTMLElement).style.height = `${window.innerHeight}px`;
-    };
-    window.addEventListener('resize', onResize);
-    return () => window.removeEventListener('resize', onResize);
   }, []);
 
   const containerStyle = {
@@ -31,7 +25,6 @@ const SingleContentLayout:React.FC<SingleContentInterface> = ({ children }) => {
   };
   return (
     <div className='single-content-layout scroll' style={containerStyle}> 
-    <div className='singleContentLayout scroll' style={{height: `${window.innerHeight}px`}}> 
         {children}
     </div>
   )
