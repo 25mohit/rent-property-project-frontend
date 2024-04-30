d8f9ecef482f5dea9c2b54997fd0424b1f4bc220
rb7878 <jkdesigner62@gmail.com>
2024-04-30 22:26:57 +0530

    chat details screen complete

--- src/app/ChatDetails/page.tsx
+++ src/app/ChatDetails/page.tsx
@@ -7,7 +7,7 @@ import { prImg1, prImg2, prImg3, profileImg } from "../../../public/images";
 import Image from "next/image";
 import { GoLocation } from 'react-icons/go';
 import Link from 'next/link';
-import { FaCircleCheck } from 'react-icons/fa6';
+import { FaCircleCheck, FaPaperPlane } from 'react-icons/fa6';
 import { IoIosArrowForward } from 'react-icons/io';
 import { TbDiscountCheckFilled } from 'react-icons/tb';
 
@@ -22,14 +22,15 @@ const ChatDetails = () => {
   return (
     <>
       <SingleContentLayout>
-        <div className='spaceLessDetails'>
+        <div className='spaceLessDetailsChat'>
           <div className="headerBar detailsHeader chatDetailsHeader active">
             <div className="col leftMenu">
-              <button className="btn menuBtn"><IoArrowBack /></button>
+              <Link href="/Chat" className="btn menuBtn"><IoArrowBack /></Link>
               <div className='userImage'>
                 <Image src={prImg1} alt="property" className="imgBox" />
                 <Image src={profileImg} alt="property" className="userProfile" />
               </div>
+              <h5 className='dTitle textLimit l1'>Toyota Etios Liva G SP*, 2011, CNG & Hybrids</h5>
             </div>
             <div className="col rightMenu">
               <ul className="headerAction">
@@ -47,7 +48,53 @@ const ChatDetails = () => {
             </div>
           </div>
 
-      
+          <div className='priceLocation'>
+            <h5 className='location textLimit l1'>Airport Road, Ahmedabad</h5>
+            <h5 className='price textLimit l1'>₹ 2,25,000</h5>
+          </div>
+
+          <div className='chartUserBody'>
+            <div className='timeBox'><span>30-04-2024</span></div>
+            <div className='rightMgs'>
+              <p>Hello, How are you?
+                <time>11:08 pm</time>
+              </p>
+            </div>
+            <div className='leftMgs'><p>Fine, and you?
+              <time>11:08 pm</time></p></div>
+            <div className='rightMgs'><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              <time>11:08 pm</time></p></div>
+            <div className='leftMgs'><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              <time>11:08 pm</time></p></div>
+            <div className='rightMgs'>
+              <p>Hello, How are you?
+                <time>11:08 pm</time>
+              </p>
+            </div>
+            <div className='timeBox'><span>30-04-2024</span></div>
+            <div className='leftMgs'><p>Fine, and you?
+              <time>11:08 pm</time></p></div>
+            <div className='rightMgs'><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              <time>11:08 pm</time></p></div>
+            <div className='leftMgs'><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              <time>11:08 pm</time></p></div>
+            <div className='rightMgs'>
+              <p>Hello, How are you?
+                <time>11:08 pm</time>
+              </p>
+            </div>
+            <div className='leftMgs'><p>Fine, and you?
+              <time>11:08 pm</time></p></div>
+            <div className='rightMgs'><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              <time>11:08 pm</time></p></div>
+            <div className='leftMgs'><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              <time>11:08 pm</time></p></div>
+          </div>
+
+          <div className='submitForm'>
+            <input type="text" className='form-control' placeholder='Message' />
+            <button className='btn'><FaPaperPlane /></button>
+          </div>
         </div>
       </SingleContentLayout>
     </>
