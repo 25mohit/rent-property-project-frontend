1d9d27d20254679f24158c9b5ec54e1ce1d84dd7
25mohit <mohit724agarwal@gmail.com>
2024-05-02 20:45:58 +0530

    Taking Pull

--- src/app/ItemDetails/page.tsx
+++ src/app/ItemDetails/page.tsx
@@ -151,8 +151,6 @@ const ItemDetails = () => {
               <button className='mainBtn fillBtn'>Chat</button>
             </div>
           </div>
-
-
           <div className='googleMap'>
           <iframe className='MapBox' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227748.43601861503!2d75.62574093351257!3d26.885421393350004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4adf4c57e281%3A0xce1c63a0cf22e09!2sJaipur%2C%20Rajasthan!5e0!3m2!1sen!2sin!4v1713976333520!5m2!1sen!2sin"></iframe>
           </div>
