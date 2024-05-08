74c93b03feccdc10683aea1298bd860fea5d50c9
jitendra <109736020+rb7878@users.noreply.github.com>
2024-05-08 08:37:16 +0000

    input field ID issue resolve

--- src/app/components/Utils/Form/InputField/InputField.tsx
+++ src/app/components/Utils/Form/InputField/InputField.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiFillEye } from 'react-icons/ai';
 
 interface InputInterface{
@@ -13,16 +13,27 @@ interface InputInterface{
 }
 const InputField:React.FC<InputInterface> = ({ type, placeholder, min, max, field, label, verifyClick }) => {
     const [hasValue, setHasValue] = useState(false);
+    const [newID, setNewID] = useState('');
 
     const handleInputChange = (event: any) => {
         setHasValue(event.target.value !== '');
     };
+
+    useEffect(() => {
+      function generateRandomNumber() {
+        const currentTime = Date.now(); // Get the current timestamp
+        const randomNumber = Math.floor(Math.random() * currentTime); // Generate a random number based on the current timestamp
+        return `${randomNumber}`;
+      }
+      setNewID(generateRandomNumber())
+    },[])
     
+    // console.log("generateRandomNumber", generateRandomNumber())
   return (
-    <div className='formGroup'>
+    <div className='formGroup'> 
       <div className='formGroupMain'>
-        <input type={type} id='field-input' className={`${field === "mobile" ? 'form-control' : 'form-control'}`} min={min} max={max} onChange={handleInputChange}/>
-        <label htmlFor="field-input" className={hasValue ? 'formLabel active' : 'formLabel'}>{placeholder}</label>
+        <input type={type} id={newID} className={`form-control ${field === "mobile" ? 'phoneNumberSpace' : ''}`} min={min} max={max} onChange={handleInputChange}/>
+        <label htmlFor={newID} className={hasValue ? 'formLabel active' : 'formLabel'}>{placeholder}</label>
         { field === "mobile" && <span className='countryCode'>+91</span>}
         { type === "password" && <button type="button" className="eyeAction"><AiFillEye id="show-pass-btn"/></button> }
         { type === "email" && <button className='getOtpBtn' id="verify-btn" onClick={() => verifyClick && verifyClick(prev => !prev)}>Get OTP</button> }
