af63f4d19691c753398bb3cc0ae25f2871110f99
jitendra <109736020+rb7878@users.noreply.github.com>
2024-05-10 13:30:42 +0000

    price filter remove in this code

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -6,7 +6,9 @@ import { electronic, furniture, property } from "../../../public/images";
 import PropertyCard from "../components/Utils/PropertyCard/PropertyCard";
 import { MdFilterAlt } from "react-icons/md";
 
-
+function valuetext(value: number) {
+    return `${value}°C`;
+}
 const HomePage = () => {
 
     const [activeTab, setActiveTab] = useState(2);
@@ -21,6 +23,8 @@ const HomePage = () => {
         setIsPopupActive(!isPopupActive);
     };
 
+
+  
     return (
 
         <MultiContentLayout>
@@ -88,37 +92,39 @@ const HomePage = () => {
 
 
             <div className={`bottomPopup ${isPopupActive ? 'active' : ''}`}>
-                    <div className='overlayBg' onClick={togglePopup}></div>
-                    <div className='bottomPopupBody'>
-                        <div className='filterHead'>
-                            <h5>Filter</h5>
+                <div className='overlayBg' onClick={togglePopup}></div>
+                <div className='bottomPopupBody'>
+                    <div className='filterHead'>
+                        <h5>Filter</h5>
+                    </div>
+                    <div className='filterTabbing'>
+                        <div className='left'>
+                            <ul>
+                                <li onClick={() => handleTabClick(1)} className={activeTab === 1 ? 'active' : ''} id="tabId1">By Budget</li>
+                                <li onClick={() => handleTabClick(2)} className={activeTab === 2 ? 'active' : ''} id="tabId2">By Sort</li>
+                            </ul>
                         </div>
-                        <div className='filterTabbing'>
-                            <div className='left'>
-                                <ul>
-                                    <li onClick={() => handleTabClick(1)} className={activeTab === 1 ? 'active' : ''} id="tabId1">By Budget</li>
-                                    <li onClick={() => handleTabClick(2)} className={activeTab === 2 ? 'active' : ''} id="tabId2">By Sort</li>
-                                </ul>
-                            </div>
-                            <div className='right'>
-                                <div className='tabContent'>
-                                    <div className='tabPane' id="tabPaneBody1" style={{ display: activeTab === 1 ? 'block' : 'none' }}>sd</div>
-                                    <div className='tabPane' id="tabPaneBody2" style={{ display: activeTab === 2 ? 'block' : 'none' }}>
-                                        <ul className='sortOption'>
-                                            <li className='active'>Price : Low to High</li>
-                                            <li>Price : High to Low</li>
-                                        </ul>
-                                    </div>
-
+                        <div className='right'>
+                            <div className='tabContent'>
+                                <div className='tabPane' id="tabPaneBody1" style={{ display: activeTab === 1 ? 'block' : 'none' }}>
+                                   
                                 </div>
+                                <div className='tabPane' id="tabPaneBody2" style={{ display: activeTab === 2 ? 'block' : 'none' }}>
+                                    <ul className='sortOption'>
+                                        <li className='active'>Price : Low to High</li>
+                                        <li>Price : High to Low</li>
+                                    </ul>
+                                </div>
+
                             </div>
-                        </div>
-                        <div className='fixFilterAction'>
-                            <button className='mainBtn borderBtn' onClick={togglePopup}>Clear all</button>
-                            <button className='mainBtn fillBtn' onClick={togglePopup}>Filter</button>
                         </div>
                     </div>
+                    <div className='fixFilterAction'>
+                        <button className='mainBtn borderBtn' onClick={togglePopup}>Clear all</button>
+                        <button className='mainBtn fillBtn' onClick={togglePopup}>Filter</button>
+                    </div>
                 </div>
+            </div>
 
         </MultiContentLayout>
 
