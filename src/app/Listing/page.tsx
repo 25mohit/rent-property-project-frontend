56a2bb7fcabdd8f28894f9f1ad2e3b1072d7adb8
jitendra <109736020+rb7878@users.noreply.github.com>
2024-05-13 11:33:31 +0000

    listing page loader

--- src/app/Listing/page.tsx
+++ src/app/Listing/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import MultiContentLayout from "../components/HOC/Layout/MultiContentLayout";
 import Image from "next/image";
 import MyAds from "../components/Utils/MyAds/MyAds";
+import ListingLoader from "../components/Utils/SkeletonLoader/ListingLoader";
 
 const Listing = () => {
   return (
    
     <MultiContentLayout>
+      <ListingLoader />
       <div className="row listingProduct bottomSpace">
           <div className="titleBox">
             <div className="col"><h3 className="titleS">My Ads</h3></div>
---
+++ src/app/components/Utils/SkeletonLoader/ListingLoader.tsx
@@ -0,0 +1,152 @@
+
+
+const ListingLoader = () => {
+    return (
+
+        <div className="row listingProduct bottomSpace skeletonLoader">
+            <div className="titleBox">
+                <div className="col"><h3 className="titleS height20 width150 skeletonAnimation"></h3></div>
+            </div>
+            <div className="col-12">
+                <div className="myAdsCard">
+                    <div className="myAdsCardHead">
+                        <h3 className="height10 width150 skeletonAnimation"></h3>
+                    </div>
+                    <div className="myAdsCardIn">
+                        <figure className="productCardImg">
+                            <div className="img skeletonAnimation" style={{ width: '100%' }}></div>
+                        </figure>
+                        <div className="productCardBoy">
+                            <h3 className="itemPrice textLimit l1 height20 width200 skeletonAnimation"></h3>
+                            <h4 className="distance height10 width150 skeletonAnimation"></h4>
+                            <h5 className="prdTitle textLimit l1 height10 width200 skeletonAnimation"></h5>
+                            <p className="counts"></p>
+                        </div>
+
+                    </div>
+
+                    <div className="productCardBottom">
+                        <p className="height10 width200 skeletonAnimation"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="myAdsCard">
+                    <div className="myAdsCardHead">
+                        <h3 className="height10 width150 skeletonAnimation"></h3>
+                    </div>
+                    <div className="myAdsCardIn">
+                        <figure className="productCardImg">
+                            <div className="img skeletonAnimation" style={{ width: '100%' }}></div>
+                        </figure>
+                        <div className="productCardBoy">
+                            <h3 className="itemPrice textLimit l1 height20 width200 skeletonAnimation"></h3>
+                            <h4 className="distance height10 width150 skeletonAnimation"></h4>
+                            <h5 className="prdTitle textLimit l1 height10 width200 skeletonAnimation"></h5>
+                            <p className="counts"></p>
+                        </div>
+
+                    </div>
+
+                    <div className="productCardBottom">
+                        <p className="height10 width200 skeletonAnimation"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="myAdsCard">
+                    <div className="myAdsCardHead">
+                        <h3 className="height10 width150 skeletonAnimation"></h3>
+                    </div>
+                    <div className="myAdsCardIn">
+                        <figure className="productCardImg">
+                            <div className="img skeletonAnimation" style={{ width: '100%' }}></div>
+                        </figure>
+                        <div className="productCardBoy">
+                            <h3 className="itemPrice textLimit l1 height20 width200 skeletonAnimation"></h3>
+                            <h4 className="distance height10 width150 skeletonAnimation"></h4>
+                            <h5 className="prdTitle textLimit l1 height10 width200 skeletonAnimation"></h5>
+                            <p className="counts"></p>
+                        </div>
+
+                    </div>
+
+                    <div className="productCardBottom">
+                        <p className="height10 width200 skeletonAnimation"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="myAdsCard">
+                    <div className="myAdsCardHead">
+                        <h3 className="height10 width150 skeletonAnimation"></h3>
+                    </div>
+                    <div className="myAdsCardIn">
+                        <figure className="productCardImg">
+                            <div className="img skeletonAnimation" style={{ width: '100%' }}></div>
+                        </figure>
+                        <div className="productCardBoy">
+                            <h3 className="itemPrice textLimit l1 height20 width200 skeletonAnimation"></h3>
+                            <h4 className="distance height10 width150 skeletonAnimation"></h4>
+                            <h5 className="prdTitle textLimit l1 height10 width200 skeletonAnimation"></h5>
+                            <p className="counts"></p>
+                        </div>
+
+                    </div>
+
+                    <div className="productCardBottom">
+                        <p className="height10 width200 skeletonAnimation"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="myAdsCard">
+                    <div className="myAdsCardHead">
+                        <h3 className="height10 width150 skeletonAnimation"></h3>
+                    </div>
+                    <div className="myAdsCardIn">
+                        <figure className="productCardImg">
+                            <div className="img skeletonAnimation" style={{ width: '100%' }}></div>
+                        </figure>
+                        <div className="productCardBoy">
+                            <h3 className="itemPrice textLimit l1 height20 width200 skeletonAnimation"></h3>
+                            <h4 className="distance height10 width150 skeletonAnimation"></h4>
+                            <h5 className="prdTitle textLimit l1 height10 width200 skeletonAnimation"></h5>
+                            <p className="counts"></p>
+                        </div>
+
+                    </div>
+
+                    <div className="productCardBottom">
+                        <p className="height10 width200 skeletonAnimation"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="myAdsCard">
+                    <div className="myAdsCardHead">
+                        <h3 className="height10 width150 skeletonAnimation"></h3>
+                    </div>
+                    <div className="myAdsCardIn">
+                        <figure className="productCardImg">
+                            <div className="img skeletonAnimation" style={{ width: '100%' }}></div>
+                        </figure>
+                        <div className="productCardBoy">
+                            <h3 className="itemPrice textLimit l1 height20 width200 skeletonAnimation"></h3>
+                            <h4 className="distance height10 width150 skeletonAnimation"></h4>
+                            <h5 className="prdTitle textLimit l1 height10 width200 skeletonAnimation"></h5>
+                            <p className="counts"></p>
+                        </div>
+
+                    </div>
+
+                    <div className="productCardBottom">
+                        <p className="height10 width200 skeletonAnimation"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListingLoader
