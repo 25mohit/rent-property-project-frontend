c477abc29c72e86640a83629ba0a8c49ec17bb22
rb78781 <jkdesigner62+1@gmail.com>
2024-05-16 08:58:59 +0000

    splash

--- src/app/Splash/page.tsx
+++ src/app/Splash/page.tsx
@@ -1,18 +1,28 @@
 'use client'
 import React from "react";
-import Image from "next/image";
 import LogoImage from "../components/Utils/SvgIcons/LogoImage";
 import SingleContentLayout from "../components/HOC/Layout/SingleContentLayout";
 
+import { useEffect } from 'react';
+import { useRouter } from "next/navigation";
+
 const Splash = () => {
+  const router = useRouter();
+
+  // useEffect(() => {
+
+  //    setTimeout(() => {
+  //     router.push('/Home');
+  //   }, 3000); 
+
+  // }, []); 
+
   return (
-    <SingleContentLayout>
       <div className="splashSection">
         <div className="splashBody">
           <LogoImage />
         </div>
       </div>
-    </SingleContentLayout>
   );
 };
 
