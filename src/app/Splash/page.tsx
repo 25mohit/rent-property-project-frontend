99575ae92845c72fbf22e9a7ab654bd0d36dcfdd
jitendra <109736020+rb7878@users.noreply.github.com>
2024-05-16 05:41:42 +0000

    splash

---
+++ src/app/Splash/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from "react";
+import Image from "next/image";
+
+const Splash = () => {
+  return (
+    <div className="splashSection">
+        <div className="splashBody">
+            <p>Loading</p>      
+        </div>  
+    </div>
+  );
+};
+
+export default Splash;
