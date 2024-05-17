e9c45502970300c7850d5724f349ff951cc30a86
rb7878 <jkdesigner62@gmail.com>
2024-05-17 21:40:05 +0530

    refer

---
+++ src/app/Transition/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+import React from "react";
+import SingleContentLayout from "../components/HOC/Layout/SingleContentLayout";
+import { IoArrowBack } from "react-icons/io5";
+import { useRouter } from "next/navigation";
+
+const Transition = () => {
+    const router = useRouter()
+const handleBack = () => {
+	router.back()
+}
+
+    return (
+
+        <SingleContentLayout>
+            <div className="headerBar innerHeader">
+                <div className="col"><button className="btn backBtn" onClick={handleBack}><IoArrowBack /></button>
+                </div>
+                <div className="col centerTitle">
+                    <h5 className='dTitle textLimit l1'>Transition</h5>
+                </div>
+                <div className="col"></div>
+            </div>
+
+            <div className="defaultSpace">
+                
+            </div>
+        </SingleContentLayout>
+
+    );
+};
+
+export default Transition;
