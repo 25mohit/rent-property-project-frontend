29d73723a3a841d62587831f4c15b1cbb29c7d2d
rb78781 <jkdesigner62+1@gmail.com>
2024-05-17 10:10:21 +0000

    refer page

--- src/app/Refer/page.tsx
+++ src/app/Refer/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa6";
 import ReferImage from "../components/Utils/SvgIcons/ReferImage";
 import CalculateFontSize from "../components/HOC/Layout/CalculateFontSize";
+import { MdOutlineCurrencyRupee } from "react-icons/md";
 
 
 const Refer = () => {
@@ -14,31 +15,36 @@ const Refer = () => {
     return (
 
         <CalculateFontSize>
-        <SingleContentLayout>
-            <div className="headerBar innerHeader">
-                <div className="col">
-                    <Link href="/SelectAddress" className="btn backBtn"><IoArrowBack /></Link>
-                </div>
-                <div className="col centerTitle">
-                    <h5 className='dTitle textLimit l1'>Refer & Rewards</h5>
-                </div>
-                <div className="col"></div>
-            </div>
-
-            <div className="defaultSpace">
-                <Link href="/" className="referCount">
-                    <div className="left">
-                        <p>Total Refers</p>
-                        <h3 className="count">20</h3>
+            <SingleContentLayout>
+                <div className="headerBar innerHeader">
+                    <div className="col">
+                        <Link href="/SelectAddress" className="btn backBtn"><IoArrowBack /></Link>
                     </div>
-                    <FaArrowRight className="arrow" />
-                </Link>
-
-                <div className="referBox">
-                   <ReferImage />
+                    <div className="col centerTitle">
+                        <h5 className='dTitle textLimit l1'>Refer & Rewards</h5>
+                    </div>
+                    <div className="col"></div>
                 </div>
-            </div>
-        </SingleContentLayout>
+
+                <div className="defaultSpace">
+                    <Link href="/" className="referCount">
+                        <div className="left">
+                            <p>Total Refers</p>
+                            <h3 className="count">20</h3>
+                        </div>
+                        <FaArrowRight className="arrow" />
+                    </Link>
+
+                    <div className="referBox">
+                        <ReferImage />
+                        <div className="referBoxBody">
+                            <h5>Get 10 <MdOutlineCurrencyRupee className="icon" /></h5>
+                            <h6>For every new user you refer</h6>
+                            <p>Share you referral link and earn 10 <MdOutlineCurrencyRupee className="icon" /></p>
+                        </div>
+                    </div>
+                </div>
+            </SingleContentLayout>
         </CalculateFontSize>
     );
 };
